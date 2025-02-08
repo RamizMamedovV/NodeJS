f95898de510f507dd6f98fbd8f5b94f684490ff2
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2025-02-08 17:15:27 +0400

    working with fs, fs.promise, path

---
+++ Sem3_path_json_url/writePerson.js
@@ -0,0 +1,44 @@
+/**
+ * 
+const fs = require('fs');
+const path = require('path');
+
+const person = {
+    name: "Ivan",
+    surName: "Ivanov",
+    age: 45,
+    sity: "London",
+}
+
+const personJson = JSON.stringify(person, null, 2);
+fs.writeFileSync(path.join(__dirname, 'person.json'), personJson);
+*/
+
+
+// Альтернативный вариант с async/await
+// Для более удобной работы с Promise можно использовать синтаксис async/await:
+
+const fs = require('fs').promises;
+const path = require('path');
+
+const person = {
+    name: "Ivan",
+    surName: "Ivanov",
+    age: 45,
+    sity: "London",
+};
+
+const personJson = JSON.stringify(person, null, 2);
+
+// Асинхронная функция для записи файла
+async function savePersonToFile() {
+    try {
+        await fs.writeFile(path.join(__dirname, 'person.json'), personJson);
+        console.log('Файл успешно записан!');
+    } catch (err) {
+        console.error('Ошибка при записи файла:', err);
+    }
+}
+
+// Вызов асинхронной функции
+savePersonToFile();
